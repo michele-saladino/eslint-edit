b0b1539763067afabf6e65a39580abf5352a4094
michele saladino <michele.saladino@movesolutions.it>
2026-06-18 14:58:09 +0200

    logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-import { getChangedLines } from "./git.js";
+import type { ESLint } from "eslint";
+import { runEslint } from "./eslint.js";
+import { getChangedLines, type FileChange } from "./git.js";
 
-getChangedLines().then(console.log);
+async function checkEslintOnChangedLines() {
+  let problems: ESLint.LintResult[] = [];
+  let changedFiles: FileChange[] = [];
+  let newProblems: ESLint.LintResult[] = [];
+
+  try {
+    const res = await runEslint();
+    problems = res.results;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    changedFiles = await getChangedLines();
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  // check if any error regards something in the changed lines
+  for (const problem of problems) {
+    const changedFile = changedFiles.find((c) => c.file === problem.filePath);
+
+    if (changedFile) {
+      const problemMatch = problem.messages.some((p) =>
+        changedFile.lines.includes(p.line),
+      );
+      if (problemMatch) {
+        newProblems.push(problem);
+      }
+    }
+  }
+
+  console.log(newProblems);
+}
+
+checkEslintOnChangedLines();
